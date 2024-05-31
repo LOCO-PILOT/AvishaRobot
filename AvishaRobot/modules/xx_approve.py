4b846927d237371b256eb0f48465c24104ee3b0f
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-05-31 16:16:28 +0530

    Create xx_approve.py

---
+++ AvishaRobot/modules/xx_approve.py
@@ -0,0 +1,30 @@
+from AvishaRobot import pbot as app
+from os import environ
+from pyrogram import Client, filters
+from pyrogram.types import ChatJoinRequest
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+EVAA = [
+    [
+        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/avishaxbot?startgroup=true"),
+    ],
+]
+
+# Extract environment variables or provide default values
+chat_id_env = environ.get("CHAT_ID")
+CHAT_ID = [int(app) for app in chat_id_env.split(",")] if chat_id_env else []
+
+TEXT = environ.get("APPROVED_WELCOME_TEXT", "❅ ʜᴇʟʟᴏ ʙᴀʙʏ ➥ {mention} ♥︎\n\n❅ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ➥ {title}\n\n")
+APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
+
+# Define an event handler for chat join requests
+@app.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+async def autoapprove(client: app, message: ChatJoinRequest):
+    chat = message.chat  # Chat
+    user = message.from_user  # User
+    print(f"✦ {user.first_name} ᴊᴏɪɴᴇᴅ ♥︎")  # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    if APPROVED == "on":
+        await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title),reply_markup=InlineKeyboardMarkup(EVAA),)
+
+
