5c178a6ec50859f7fc63616c020279f84ac29bdb
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-05-31 16:14:24 +0530

    Create xx_vc.py

---
+++ AvishaRobot/modules/xx_vc.py
@@ -0,0 +1,53 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message 
+from AvishaRobot import pbot as app
+from AvishaRobot import OWNER_ID
+
+
+
+# vc on
+@app.on_message(filters.video_chat_started)
+async def brah(_, msg):
+       await msg.reply("⬤ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ sᴛᴀʀᴛᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.")
+# vc off
+@app.on_message(filters.video_chat_ended)
+async def brah2(_, msg):
+       await msg.reply("⬤ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.")
+
+# invite members on vc
+@app.on_message(filters.video_chat_members_invited)
+async def brah3(app :app, message:Message):
+           text = f"⬤ {message.from_user.mention} ɪɴᴠɪᴛᴇᴅ "
+           x = 0
+           for user in message.video_chat_members_invited.users:
+             try:
+               text += f" [{user.first_name}](tg://user?id={user.id}) "
+               x += 1
+             except Exception:
+               pass
+           try:
+             await message.reply(f"{text} ᴛᴏ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ♥︎")
+           except:
+             pass
+
+
+####
+
+@app.on_message(filters.command("math", prefixes="/"))
+def calculate_math(client, message):   
+    expression = message.text.split("/math ", 1)[1]
+    try:        
+        result = eval(expression)
+        response = f"⬤ ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs ➥ {result}"
+    except:
+        response = "⬤ ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
+    message.reply(response)
+
+###
+@app.on_message(filters.command("leavegroup")& filters.user(OWNER_ID))
+async def bot_leave(_, message):
+    chat_id = message.chat.id
+    text = f"⬤ ʙᴏᴛ sᴜᴄᴄᴇssғᴜʟʟʏ ʟᴇғᴛ ᴛʜᴇ ɢʀᴏᴜᴘ."
+    await message.reply_text(text)
+    await app.leave_chat(chat_id=chat_id, delete=True)
+
