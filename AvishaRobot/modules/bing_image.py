1f910cdd58e8c6d08e2a5d1eb34943da0f20bc85
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-06-03 15:08:16 +0530

    Create bing_image.py

---
+++ AvishaRobot/modules/bing_image.py
@@ -0,0 +1,52 @@
+import json
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import InputMediaPhoto, Message
+from MukeshRobot import pbot as app
+
+
+@app.on_message(filters.command("bimg"))
+def bingimg_search(client: Client, message: Message):
+    try:
+        text = message.text.split(None, 1)[
+            1
+        ]  
+    except IndexError:
+        return message.reply_text(
+            "❖ ᴘʀᴏᴠɪᴅᴇ ᴍᴇ ᴀ ǫᴜᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ!"
+        )  
+
+    search_message = message.reply_text(
+        "⚡"
+    )  
+
+    url = "https://sugoi-api.vercel.app/bingimg?keyword=" + text
+    resp = requests.get(url)
+    images = json.loads(resp.text)
+
+    media = []
+    count = 0
+    for img in images:
+        if count == 6:
+            break
+
+        media.append(InputMediaPhoto(media=img))
+        count += 1
+
+    message.reply_media_group(media=media)
+
+    search_message.delete()
+    message.delete()
+####
+
+__mod_name__ = "ɪᴍɢ"
+
+__help__ = """
+
+ ⬤ /bimg ➥ sᴇᴀʀᴄʜ ᴘɪɴᴛᴇʀᴇsᴛ ɪᴍᴀɢᴇs ᴄᴏʟʟᴇᴄᴛɪᴏɴ.
+ ⬤ /img ➥ sᴇᴀʀᴄʜ ɢᴏᴏɢʟᴇ ɪᴍᴀɢᴇs ᴄᴏʟʟᴇᴄᴛɪᴏɴ.
+ ⬤ /pimg ➥ sᴇᴀʀᴄʜ ᴘʀᴇᴍɪᴜᴍ ɪᴍᴀɢᴇs ᴄᴏʟʟᴇᴄᴛɪᴏɴ, ᴜsᴇʀs ᴜsᴇ ᴏɴʟʏ ᴍʏ ᴘᴍ.
+ ⬤ /rp ➥ ʀᴀɴᴅᴏᴍ ᴡᴀʟʟᴘᴀᴘᴇʀ ɪᴍᴀɢᴇ.
+ ⬤ /pic <query> ➥ sᴇᴀʀᴄʜ ᴏᴡɴ ǫᴜᴇʀʏ ɪᴍᴀɢᴇ [● ᴇx ✒ /pic Tajmahal].
+ """
+
