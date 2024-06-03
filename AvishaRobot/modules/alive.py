0672ea2fe5a032d4fea6b25bff0c574ebe0089c4
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-06-03 16:01:21 +0530

    Create alive.py

---
+++ AvishaRobot/modules/alive.py
@@ -0,0 +1,77 @@
+import random
+import asyncio
+from platform import python_version as pyver
+
+from pyrogram import __version__ as pver
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from telegram import __version__ as lver
+from telethon import __version__ as tver
+
+from AvishaRobot import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
+
+MISHI = [
+    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
+    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
+    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
+    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
+    "https://graph.org/file/84e84ff778b045879d24f.jpg",
+    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
+    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
+    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
+    "https://graph.org/file/37248e7bdff70c662a702.jpg",
+    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
+    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
+    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
+    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
+    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
+    "https://graph.org/file/39d7277189360d2c85b62.jpg",
+    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
+    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
+    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+]
+
+Avisha = [
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/roy_editx")
+    ],
+]
+
+
+
+@pbot.on_message(filters.command("alive"))
+async def restart(client, m: Message):
+    await m.delete()
+    accha = await m.reply("🤍")
+    await asyncio.sleep(0.2)
+    await accha.edit("🖤")
+    await asyncio.sleep(0.1)
+    await accha.edit("🧡")
+    await asyncio.sleep(0.1)
+    await accha.edit("💚")
+
+    await accha.delete()
+    await asyncio.sleep(0.3)
+    umm = await m.reply_sticker(
+        "CAACAgUAAxkDAAJHbmLuy2NEfrfh6lZSohacEGrVjd5wAAIOBAACl42QVKnra4sdzC_uKQQ"
+    )
+    await umm.delete()
+    await asyncio.sleep(0.2)
+    await m.reply_photo(
+        random.choice(MISHI),
+        caption=f"""**❖ ʜᴇʏ, ɪ ᴀᴍ [{BOT_NAME}](f"t.me/{BOT_USERNAME}") **\n\n● **ʟɪʙʀᴀʀʏ ➥** `{lver}`\n● **ᴛᴇʟᴇᴛʜᴏɴ ➥** `{tver}`\n● **ᴘʏʀᴏɢʀᴀᴍ ➥** `{pver}`\n● **ᴘʏᴛʜᴏɴ ➥** `{pyver()}`\n\n❖ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥** [๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐](tg://user?id={OWNER_ID})""",
+        reply_markup=InlineKeyboardMarkup(Avisha),
+    )
+    
+__mod_name__ = "ᴀʟɪᴠᴇ"
+
+__help__ = """
+
+ ⬤ /alive ➥ ᴄʜᴇᴄᴋ ʙᴏᴛ ᴀʟɪᴠᴇ sᴛᴀᴛᴜs.
+ ⬤ /ping ➥ ᴄʜᴇᴄᴋ ᴘɪɴɢ sᴛᴀᴛᴜs.
+ ⬤ /stats ➥ ᴄʜᴇᴄᴋ ʙᴏᴛ sᴛᴀᴛs.
+ ⬤ /pingall ➥ ᴄʜᴇᴄᴋ ᴘɪɴɢ sᴛᴀᴛᴜs ᴏғ ᴀʟʟ ᴍᴏᴅᴜʟᴇs.
+ """
+    
