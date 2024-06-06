f1f43f45b4e4298d95dc76a2434fb6f3afcedb5d
『ᴀғᴋ』﹤/3 『ᴏʏᴇ_ᴄʀᴜsʜ』 <146160310+tinaarobot@users.noreply.github.com>
2024-06-06 15:51:34 +0530

    Update guess.py

--- AvishaRobot/modules/guess.py
+++ AvishaRobot/modules/guess.py
@@ -11,8 +11,8 @@ async def wish(e):
          mm = random.randint(1,100)
          lol = await e.get_reply_message()
          fire = "https://telegra.ph/file/060b79fec4a50f48d59ba.jpg"
-         await neko.send_file(e.chat_id, fire,caption=f"**⬤ ʜᴇʏ ʙᴀʙʏ [{e.sender.first_name}](tg://user?id={e.sender.id}), ʏᴏᴜʀ ɢᴜᴇss ɪs {mm}%__**\n\n__Correct!!!", reply_to=lol)
+         await neko.send_file(e.chat_id, fire,caption=f"**⬤ ʜᴇʏ ʙᴀʙʏ [{e.sender.first_name}](tg://user?id={e.sender.id}), ʏᴏᴜʀ ɢᴜᴇss ɪs {mm}%**\n\n⬤ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ᴠ ɪ s ʜ ᴀ ♡゙](https://t.me/the_friendz)", reply_to=lol)
  if not e.is_reply:
          mm = random.randint(1,100)
          fire = "https://telegra.ph/file/060b79fec4a50f48d59ba.jpg"
-         await neko.send_file(e.chat_id, fire,caption=f"**⬤ ʜᴇʏ ʙᴀʙʏ [{e.sender.first_name}](tg://user?id={e.sender.id}), ʏᴏᴜʀ ɢᴜᴇss ɪs {mm}%__**\n\n__Correct!!!", reply_to=e)
+         await neko.send_file(e.chat_id, fire,caption=f"**⬤ ʜᴇʏ ʙᴀʙʏ [{e.sender.first_name}](tg://user?id={e.sender.id}), ʏᴏᴜʀ ɢᴜᴇss ɪs {mm}%**\n\n⬤ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [๛ᴀ ᴠ ɪ s ʜ ᴀ ♡゙](https://t.me/the_friendz)", reply_to=e)
